8f5168258a9d86a0f71800e62eb31cd0205f8c0f
sandipmodi <57409950+sandipmodi@users.noreply.github.com>
2025-11-24 13:37:17 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Krish";
+        String age = "15";
+        String food = "pizza";
+        String drink = "coke";
+        String school = "NGS";
+        String grade = "10";
+
+        System.out.println ("Hello my name is " + name);
+
+        System.out.println ("I am " + age + " years old");
+
+        System.out.println ("My favorite food is " + food + " and my favorite drink is " + drink);
+
+        System.out.println ("Hi, I'm " + name );
+        System.out.println ("My school is " + school );
+        System.out.println ("My grade is " + grade );
+
+        System.out.println(5 + 5);
+    }
+}
+
